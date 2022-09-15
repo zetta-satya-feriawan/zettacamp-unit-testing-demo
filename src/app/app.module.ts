13e23bb189a7671c6ea368732be07b3924e4b419
Novan R. Kuncoro <novan.kuncoro@zettacamp.pro>
2022-09-15 11:21:31 +0700

    Update app module and explore component spec file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
 import { HttpClientModule } from '@angular/common/http'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { NgModule } from '@angular/core'
+import { ReactiveFormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { HomeComponent } from './home/home.component'
 import { ExploreComponent } from './explore/explore.component'
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, ExploreComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/explore/explore.component.spec.ts
+++ src/app/explore/explore.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { ExploreComponent } from './explore.component'
 
@@ -10,7 +11,7 @@ describe('ExploreComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ExploreComponent],
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, ReactiveFormsModule],
     }).compileComponents()
 
     fixture = TestBed.createComponent(ExploreComponent)
