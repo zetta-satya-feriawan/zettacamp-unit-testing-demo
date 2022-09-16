9c0c90256315c64d5f707e943f7766dc14c21f39
Novan R. Kuncoro <novan.kuncoro@zettacamp.pro>
2022-09-16 09:47:37 +0700

    Add new test case in `home.component.ts`

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,24 +1,45 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing'
+import { DebugElement } from '@angular/core'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { Observable, of } from 'rxjs'
+import { GifType, GiphyService } from '../services/giphy.service'
 
 import { HomeComponent } from './home.component'
 
 describe('HomeComponent', () => {
   let component: HomeComponent
   let fixture: ComponentFixture<HomeComponent>
+  let giphyServiceStub: Partial<GiphyService>
+  let de: DebugElement
 
   beforeEach(async () => {
+    giphyServiceStub = {
+      getTrendingGifs: (): Observable<GifType[]> => {
+        return of([
+          { id: 'test-id-1', src: 'test-src-1', title: 'test-title-1' },
+          { id: 'test-id-2', src: 'test-src-2', title: 'test-title-2' },
+        ])
+      },
+    }
     await TestBed.configureTestingModule({
       declarations: [HomeComponent],
       imports: [HttpClientTestingModule],
+      providers: [{ provide: GiphyService, useValue: giphyServiceStub }],
     }).compileComponents()
 
     fixture = TestBed.createComponent(HomeComponent)
     component = fixture.componentInstance
+    de = fixture.debugElement
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should display list of trending gifs', () => {
+    const el = de.nativeElement as HTMLElement
+    const gifs = el.querySelectorAll('li > img')
+    expect(gifs.length).toBe(2)
   })
 })
